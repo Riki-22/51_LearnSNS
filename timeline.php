44dc9e2500e3b48f60b3587354d7e19fcfc199c6
Riki Ogasawara <>
2019-03-08 13:33:04 +0800

    test

--- timeline.php
+++ timeline.php
@@ -1,12 +1,76 @@
 <?php
     session_start();
+    require 'dbconnect.php';
 
-    echo '<pre>';
-    var_dump($_SESSION);
-    echo '</pre>';
+    // サインインしてないユーザーのアクセス禁止
+    if (!isset($_SESSION['51_LearnSNS']['id'])) {
+        header('Location: signin.php');
+        exit();
+    }
+
+    // サインインしているユーザーの情報を取得
+    $sql = 'SELECT * FROM  `users` WHERE `id` = ?';
+    // 再任処理時にセッションに保持したユーザーのIDで絞り込む
+    $data = [$_SESSION['51_LearnSNS']['id']];
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute($data);
+
+    $signin_user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    // echo '<pre>';
+    // var_dump($signin_user);
+    // echo '</pre>';
+
+    // エラー内容保持する配列
+    $errors = [];
+
+    // POST送信されたとき
+    if (!empty($_POST)) {
+        $feed = $_POST['feed'];
+
+        // 空チェック
+        if ($feed != '') {
+            // 投稿処理
+            $sql = 'INSERT INTO `feeds` (`feed`, `user_id`, `created`) VALUES (?, ?, NOW())';
+            // 宿題１：この続き
+            $data = [$feed, $signin_user['id']];
+            // オブジェクト指向の範囲
+            // インスタンス->メンバメソッド（引数）
+            // 主体->振る舞い（利用するもの）
+            // $human->walk();
+            // $human->eat($meat);
+            $stmt = $dbh->prepare($sql);
+            $stmt->execute($data);
+            // 登録できたかはphpMyAdminで確認
+
+            // 宿題２：登録処理が終わったらタイムライン画面に遷移
+            // この処理を入れないとtimeline.phpにPOST送信で留まることになる
+            // ブラウザ更新するたびにフォームが送信されてしまう
+            header('Location: timeline.php');
+            exit();
+        } else {
+            // エラー
+            $errors['feed'] = 'blank';
+        }
+    }
+
 ?>
 <?php include 'layouts/header.php'; ?>
 <body style="margin-top: 60px; background: #E4E6EB;">
+     <!-- 
+         include(ファイル名);
+         指定されたファイルの内容をそのまま差し込む
+         同じレイアウトを複数画面から利用するときなどに利用
+         ※includeとrequireの違い
+            エラー発生時の挙動が異なる
+            include : Warningとなり、処理は続行
+            require : Errorとなり、処理は中断
+
+            不具合が発生しても重要な問題にならないものはinclude
+            DB接続など重要な機能に関してはrequire
+        
+        読み込み元で利用可能な変数は、読み込まれたファイルの中でも使用可能
+      -->
     <?php include 'navbar.php'; ?>
     <div class="container">
         <div class="row">
@@ -21,6 +85,9 @@
                     <form method="POST" action="">
                         <div class="form-group">
                             <textarea name="feed" class="form-control" rows="3" placeholder="Happy Hacking!" style="font-size: 24px;"></textarea><br>
+                            <?php if (isset($errors['feed']) && $errors['feed'] == 'blank'): ?>
+                            <p class="text-danger">投稿内容を入力してください</p>
+                            <?php endif; ?>
                         </div>
                         <input type="submit" value="投稿する" class="btn btn-primary">
                     </form>
